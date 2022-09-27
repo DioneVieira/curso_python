ad8c94e6b5550a27e65cc9ddab6968c1c2c0f4cf
DioneVieira <vieira_dione@hotmail.com>
2022-09-27 11:07:59 -0300

    Arquivos do Curso de Python

---
+++ conteudo/variaveis_e_constantes.py
@@ -0,0 +1,18 @@
+#Boas Práticas
+
+#Variáveis
+
+nome = "Dione"
+idade = 30
+
+nome, idade = "Leticia", 26
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+# Constantes
+
+BRAZILIAN_STATES = ["PR", "SP", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
